22286008ad30086963837970a6145f9903ed741d
behrooz3500 <behrooz3500@yahoo.com>
2024-01-20 20:59:50 +0330

    remove unused url patterns

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,8 +1,6 @@
-from django.urls import path
 from webapp import views
+from django.urls import path
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("viewer", views.viewer, name="viewer"),
-    path("designer", views.designer, name="designer"),
 ]
