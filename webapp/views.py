c3b52cf7838bd1d69f4afe7f4a26708f5018cffe
behrooz3500 <behrooz3500@yahoo.com>
2024-01-21 10:24:42 +0330

    use context for passing json data

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,49 @@
+import json
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'viewer.html')
+    test_data = {
+        "ReceiptInfo": {
+            "invoice_date": "1402/10/12",
+            "invoice_code": "1001",
+            "company_name": "بهروز و دوستان",
+            "company_tel": "09150207215",
+            "company_address": "مشهد، قاسم آباد",
+            "person_name": "علی کریمی",
+            "tel": "09308744204",
+            "address": "مشهد، آزادشهر",
+            "company_note": "be good",
+            "items_sum": "250000",
+            "discount": "100000",
+            "tax": "5000",
+            "total": "245000",
+            "previous_payments": "0",
+            "receipt_total": "245000"
+        },
+        "ReceiptItems":[
+            {
+                "product_name": "پفک نمکی",
+                "item_quantity": "2",
+                "item_total": "250000"
+            },
+            {
+                "product_name": "پفک نمکی",
+                "item_quantity": "8",
+                "item_total": "850000"
+            },
+            {
+                "product_name": "پفک نمکی",
+                "item_quantity": "2",
+                "item_total": "250000"
+            },
+            {
+                "product_name": "پفک نمکی",
+                "item_quantity": "2",
+                "item_total": "250000"
+            }
+        ]
+    }
+    json_data = json.dumps(test_data)
+    context = {"json_data": json_data}
+    return render(request, 'viewer.html', context=context)
 
