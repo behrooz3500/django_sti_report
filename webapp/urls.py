403e156c4499e92259f1e85505f18250b31fa00d
behrooz <behrooz3500@yahoo.com>
2024-01-21 19:49:42 +0330

    add a view for dynamic report viewing

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("single", views.single_viewer, name="single"),
+    path("dynamic", views.dynamic_viewer, name="dynamic"),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,49 +1,91 @@
 import json
 from django.shortcuts import render
 
-def home(request):
-    test_data = {
+test_data = {
         "ReceiptInfo": {
-            "invoice_date": "1402/10/12",
-            "invoice_code": "1001",
-            "company_name": "بهروز و دوستان",
-            "company_tel": "09150207215",
-            "company_address": "مشهد، قاسم آباد",
-            "person_name": "علی کریمی",
-            "tel": "09308744204",
-            "address": "مشهد، آزادشهر",
-            "company_note": "be good",
-            "items_sum": "250000",
-            "discount": "100000",
-            "tax": "5000",
-            "total": "245000",
-            "previous_payments": "0",
-            "receipt_total": "245000"
-        },
-        "ReceiptItems":[
-            {
-                "product_name": "پفک نمکی",
-                "item_quantity": "2",
-                "item_total": "250000"
-            },
-            {
-                "product_name": "پفک نمکی",
-                "item_quantity": "8",
-                "item_total": "850000"
-            },
-            {
-                "product_name": "پفک نمکی",
-                "item_quantity": "2",
-                "item_total": "250000"
-            },
-            {
-                "product_name": "پفک نمکی",
-                "item_quantity": "2",
-                "item_total": "250000"
-            }
-        ]
+    	"address": "مشهد، آزادشهر",
+		"carry_price": "15000",
+        "courier_name": "peyke motori",
+        "courier_tel": "091011011010",
+    	"company_address": "مشهد، آدرس شرکت اینجاست",
+    	"company_name": "بهروز و دوستان",
+    	"company_note": "be good",
+    	"company_tel": "09150207215",
+    	"discount": "100000",
+		"info": "اطلاعات اضافه",
+		"invoice_code": "1001",
+    	"invoice_date": "1402/10/12",
+		"invoice_time": "12:45",
+		"items_quantity": "5",
+    	"items_sum": "250000",
+		"logo_path": "/reports/logo.jpg",
+		"order_place": "بیرون سالن",
+		"person_code": "20035",
+    	"person_name": "علی کریمی",
+		"persons_number": "7",
+    	"previous_payments": "0",
+		"purchase_profit": "1000",
+    	"receipt_total": "245000",
+		"remain": "5000",
+    	"tax": "5000",
+    	"tel": "09308744204",
+    	"total": "245000",
+		"username": "آقای خاص"
+    },
+    "ReceiptItems":[
+		{
+			"item_info": "آبدار",
+			"item_price": "12500",
+			"item_quantity": "2",
+			"item_total": "250000",
+			"product_name": "1پفک نمکی",
+			"discounted_price": "9500"
+		},
+		{
+			"item_info": "آبدار",
+			"item_price": "12500",
+			"item_quantity": "4",
+			"item_total": "250000",
+			"product_name": "پفک نمکی",
+			"discounted_price": "9500"
+		},
+		{
+			"item_info": "آبدار",
+			"item_price": "12500",
+			"item_quantity": "6",
+			"item_total": "250000",
+			"product_name": "2پفک نمکی",
+			"discounted_price": "9500"
+		},
+		{
+			"item_info": "آبدار",
+			"item_price": "12500",
+			"item_quantity": "2",
+			"item_total": "250000",
+			"product_name": "3پفک نمکی",
+			"discounted_price": "9500"
+		},
+		{
+			"item_info": "آبدار",
+			"item_price": "12500",
+			"item_quantity": "2",
+			"item_total": "250000",
+			"product_name": "4پفک نمکی",
+			"discounted_price": "9500"
+		}
+    ]
     }
+
+def home(request):
+    return render(request, 'index.html')
+
+def single_viewer(request):
     json_data = json.dumps(test_data)
     context = {"json_data": json_data}
-    return render(request, 'viewer.html', context=context)
+    return render(request, 'single_viewer.html', context=context)
+
+def dynamic_viewer(request):
+    json_data = json.dumps(test_data)
+    context = {"json_data": json_data}
+    return render(request, 'dynamic_viewer.html', context=context)
 
